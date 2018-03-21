d0583dd246291df5afd052e4afe42f5f0726e8bb
cristina <yosola@gmail.com>
2018-03-21 16:48:01 -0700

    Question class finished and working in Adapter

--- app/src/main/java/com/example/android/mexicanquiz/Questionary.java
+++ app/src/main/java/com/example/android/mexicanquiz/Questionary.java
@@ -22,8 +22,23 @@ public class Questionary {
 
     public Questionary(Context context){
         String[] questionOneChoices = context.getResources().getStringArray(R.array.question1_choices);
-        String[] questionOneAnswers = {};
+        String[] questionOneAnswers = context.getResources().getStringArray(R.array.question1_answers);
         Question questionOne = new Question(context.getString(R.string.question1_question), questionOneChoices, questionOneAnswers);
+
+        String[] questionTwoChoices = {};
+        String[] questionTwoAnswers = {context.getString(R.string.question2_answer)};
+        Question questionTwo = new Question(context.getString(R.string.question2_question), questionTwoChoices, questionTwoAnswers);
+
+        String[] questionThreeChoices = context.getResources().getStringArray(R.array.question3_choices);
+        String[] questionThreeAnswers = {context.getString(R.string.question3_answer)};
+        Question questionThree = new Question(context.getString(R.string.question3_question), questionThreeChoices, questionThreeAnswers);
+
+        String[] questionFourChoices = context.getResources().getStringArray(R.array.question4_choices);
+        String[] questionFourAnswers = {context.getString(R.string.question4_answer)};
+        Question questionFour = new Question(context.getString(R.string.question4_question), questionFourChoices, questionFourAnswers);
+
+
+
     }
 
 
