57c9ed11bd3c1567662525fe525b7d48ab4668f3
cristina <yosola@gmail.com>
2018-04-03 16:32:16 -0700

    Ten questions working perfect.

--- app/src/main/java/com/example/android/mexicanquiz/Questionary.java
+++ app/src/main/java/com/example/android/mexicanquiz/Questionary.java
@@ -22,7 +22,7 @@ public class Questionary {
 
 
     private Questionary(Context context){
-        mQuestions = new Question[4];
+        mQuestions = new Question[10];
 
         String[] questionOneChoices = context.getResources().getStringArray(R.array.question1_choices);
         String[] questionOneAnswers = context.getResources().getStringArray(R.array.question1_answers);
@@ -43,6 +43,36 @@ public class Questionary {
         String[] questionFourAnswers = {context.getString(R.string.question4_answer)};
         Question questionFour = new Question(context.getString(R.string.question4_question), questionFourChoices, questionFourAnswers);
         mQuestions[3] = questionFour;
+
+        String[] questionFiveChoices = context.getResources().getStringArray(R.array.question5_choices);
+        String[] questionFiveAnswers = {context.getString(R.string.question5_answer)};
+        Question questionFive = new Question(context.getString(R.string.question5_question), questionFiveChoices, questionFiveAnswers);
+        mQuestions[4] = questionFive;
+
+        String[] questionSixChoices = {};
+        String[] questionSixAnswers = {context.getString(R.string.question6_answer)};
+        Question questionSix = new Question(context.getString(R.string.question6_question), questionSixChoices, questionSixAnswers);
+        mQuestions[5] = questionSix;
+
+        String[] questionSevenChoices = context.getResources().getStringArray(R.array.question7_choices);
+        String[] questionSevenAnswers = {context.getString(R.string.question7_answer)};
+        Question questionSeven = new Question(context.getString(R.string.question7_question), questionSevenChoices, questionSevenAnswers);
+        mQuestions[6] = questionSeven;
+
+        String[] questionEightChoices = context.getResources().getStringArray(R.array.question8_choices);
+        String[] questionEightAnswers = context.getResources().getStringArray(R.array.question8_answers);
+        Question questionEight = new Question(context.getString(R.string.question8_question), questionEightChoices, questionEightAnswers);
+        mQuestions[7] = questionEight;
+
+        String[] questionNineChoices = context.getResources().getStringArray(R.array.question9_choices);
+        String[] questionNineAnswers = {context.getString(R.string.question9_answer)};
+        Question questionNine = new Question(context.getString(R.string.question9_question), questionNineChoices, questionNineAnswers);
+        mQuestions[8] = questionNine;
+
+        String[] questionTenChoices = context.getResources().getStringArray(R.array.question10_choices);
+        String[] questionTenAnswers = {context.getString(R.string.question10_answer)};
+        Question questionTen = new Question(context.getString(R.string.question10_question), questionTenChoices, questionTenAnswers);
+        mQuestions[9] = questionTen;
     }
 
     public Question getCurrentQuestion(){
